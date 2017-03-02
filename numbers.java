41101abfcf6a7eeb322aa628913ba3462cb28b53
deethukral <deethukral@gmail.com>
2017-03-02 09:27:25 +0530

    Loop

---
+++ numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class numbers
+{
+public static void main(String args[])
+{
+int n;
+Scanner scan= new Scanner(System.in);
+System.out.println("how many numbers do you want to print?");
+n=scan.nextInt();
+
+for(int i=1;i<=n;i++)
+{
+
+System.out.println("numbers:"+i);
+}
+
+
+}
+
+
+
+
+}
